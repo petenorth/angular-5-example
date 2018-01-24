3804f2fb347c4bd99136b8753d44c9ddbfd1e11d
Peter Fry <pfry@redhat.com>
2018-01-24 18:48:08 +0000

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular/cli/plugins/karma'),
+      require('karma-junit-reporter'),
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
